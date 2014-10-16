4c55cdaa60328a123f72a1394bdd26485db9817c
DonJakeValino <x00112730@gmail.com>
2014-10-16 10:46:32 +0100

    Hello

---
+++ x00112730.sh
@@ -0,0 +1,35 @@
+#Test Case 1
+# Inputs:80 80
+#Expected Outputs: pass with distinction
+#Actual Outputs: pass
+java grades 80 80
+
+#Test Case 2
+# Inputs: 60 60
+#Expected Outputs:Pass
+#Actual Outputs:Pass
+java grades 40 60
+
+#Test Case 3
+# Inputs: 100 70
+#Expected Outputs: pass with distinction
+#Actual Outputs:pass with distinction
+java grades 100 70
+
+#Test Case 4
+# Inputs:100 39
+#Expected Outputs:Component Component Fail
+#Actual Outputs:Component fail
+java grades 100 39
+
+#Test Case 5
+# Inputs: 0 0
+#Expected Outputs: Fail
+#Actual Outputs:Component Fail
+java grades 0 0
+
+#Test Case 6
+# Inputs: 101 39
+#Expected Outputs: Invalid input
+#Actual Outputs:Invalid input
+java grades 101 39
