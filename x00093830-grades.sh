e5d912e8e59a98c1c3d288616ff157f0701f4b56
x0009830 <x00093830@gmail.com>
2014-10-16 10:58:26 +0100

    Bashy

---
+++ x00093830-grades.sh
@@ -0,0 +1,34 @@
+#test case 1
+#Inputs: 50 50
+#Expected Outputs:pass
+#Actual Outputs:fail
+java grades 50 50
+
+#test case 2
+#Inputs: 35 90
+#Expected Outputs: Component Fail
+#Actual Outputs: Component Fail
+java grades 35 90 
+#test case 3
+#Inputs: 90 39
+#Expected Outputs: Component Fail
+#Actual Outputs: Component Fail
+java grades 90 39
+
+#test case 4
+#Inputs: 80 80 
+#Expected Outputs:Pass
+#Actual Outputs:Pass
+java grades 80 80
+
+#test case 5
+#Inputs: 80 80
+#Expected Outputs:Pass with distinction
+#Actual Outputs:Pass
+java grades 80 80
+
+#test case 6
+#Inputs: -30 100
+#Expected Outputs:Invalid Input
+#Actual Outputs:Invalid Input
+java grades -30 100
