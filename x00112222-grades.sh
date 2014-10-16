cd034fbdd9290a7982a4342921b9c6b26dc270bb
x00112222 <you@example.com>
2014-10-16 11:05:58 +0100

    ggg

---
+++ x00112222-grades.sh
@@ -0,0 +1,24 @@
+# Test Case 1
+echo Inputs: 40 + 70
+# Expected Output: Pass
+java grades 40 70
+
+# Test Case 2
+echo Inputs: 40 + 60 
+# Expected Output: Component Fail
+java grades 40 60
+
+#Test Case 3
+echo Inputs: 70 + 50
+# Expected Output: Component Fail
+java grades 70 50
+
+# Test Case 4
+echo Inputs: 70 + 80
+# Expected Output: Component Fail
+java grades 70 80
+
+#Test Case 4
+echo Inputs: 30 + 40
+# Expected Output:
+java grades 30 40
