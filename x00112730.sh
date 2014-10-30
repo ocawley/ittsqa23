c7202d61af63b59ff335dc437267996e42a03138
DonJakeValino <x00112730@gmail.com>
2014-10-30 10:49:40 +0000

    x00112730-DonJake-sh

--- x00112730.sh
+++ x00112730.sh
@@ -2,34 +2,60 @@
 # Inputs:80 80
 #Expected Outputs: pass with distinction
 #Actual Outputs: pass
-java grades 80 80
+test1 = java grades_v2 80 80
+if["$test1" == "pass with distinction"]; then
+	echo "Test Case 1 Passed"
+else
+	echo "Test Case 1 Failed"
+fi
 
 #Test Case 2
 # Inputs: 60 60
 #Expected Outputs:Pass
 #Actual Outputs:Pass
-java grades 40 60
+java grades_v2 40 60
 
 #Test Case 3
 # Inputs: 100 70
 #Expected Outputs: pass with distinction
 #Actual Outputs:pass with distinction
-java grades 100 70
+java grades_v2 100 70
 
 #Test Case 4
 # Inputs:100 39
 #Expected Outputs:Component Component Fail
 #Actual Outputs:Component fail
-java grades 100 39
+java grades_v2 100 39
 
 #Test Case 5
 # Inputs: 0 0
 #Expected Outputs: Fail
 #Actual Outputs:Component Fail
-java grades 0 0
+java grades_v2 0 0
 
 #Test Case 6
 # Inputs: 101 39
 #Expected Outputs: Invalid input
 #Actual Outputs:Invalid input
-java grades 101 39
+java grades_v2 101 39
+
+
+#Test Case 1 Failed
+
+#Test Case 2 Passed
+
+#Test Case 3 Passed
+
+#Test Case 4 Failed
+
+#Test Case 5 Failed
+
+#Test Case 6 Passed
+#===================
+#Test Suite Summary:
+#===================
+
+#Passed: 3
+#Failed: 3
+
+
